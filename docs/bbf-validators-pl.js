65a190c2af6daefbb9918b073754af7919c9d017
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2016-08-20 22:48:35 +0200

    fix docs

--- docs/bbf-validators-pl.js
+++ docs/bbf-validators-pl.js
@@ -1,1 +1,64 @@
-../dist/bbf-validators-pl.js
+
+/*
+  Backbone-Forms validators PL 0.1.0
+
+  Copyright (c) 2016 Tomasz Jakub Rup
+
+  https://github.com/tomi77/backbone-forms-validators
+
+  Released under the MIT license
+ */
+(function(root, factory) {
+
+  /*
+  istanbul ignore next
+   */
+  switch (false) {
+    case !(typeof define === 'function' && define.amd):
+      define(['underscore', 'backbone-forms'], factory);
+      break;
+    case typeof exports !== 'object':
+      factory(require('underscore'), require('backbone-forms'));
+      break;
+    default:
+      factory(root._, root.Backbone.Form);
+  }
+})(this, function(_, Form) {
+  Form.validators.errMessages.nip = 'Invalid NIP';
+  Form.validators.nip = function(options) {
+    options = _.extend({
+      type: 'nip',
+      message: Form.validators.errMessages.nip
+    }, options);
+    return function(value) {
+      var control, err, sum, weights;
+      if (value.trim() === '') {
+        value = null;
+      }
+      if (value == null) {
+        return;
+      }
+      options.value = value;
+      err = {
+        type: options.type,
+        message: options.message
+      };
+      value = value.replace(/[\s-]/g, '');
+      if (!(value.length === 10 && parseInt(value, 10) > 0)) {
+        return err;
+      }
+      value = value.split('').map(function(val) {
+        return parseInt(val, 10);
+      });
+      control = value.pop();
+      weights = [6, 5, 7, 2, 3, 4, 5, 6, 7];
+      sum = _.reduce(_.zip(value, weights), function(memo, val) {
+        return memo + val[0] * val[1];
+      }, 0);
+      value = sum % 11;
+      if (value !== control) {
+        return err;
+      }
+    };
+  };
+});
--- docs/bbf-validators.js
+++ docs/bbf-validators.js
@@ -1,1 +1,71 @@
-../dist/bbf-validators.js
+
+/*
+  Backbone-Forms validators 0.1.0
+
+  Copyright (c) 2016 Tomasz Jakub Rup
+
+  https://github.com/tomi77/backbone-forms-validators
+
+  Released under the MIT license
+ */
+(function(root, factory) {
+
+  /* istanbul ignore next */
+  switch (false) {
+    case !(typeof define === 'function' && define.amd):
+      define(['underscore', 'backbone-forms'], factory);
+      break;
+    case typeof exports !== 'object':
+      factory(require('underscore'), require('backbone-forms'));
+      break;
+    default:
+      factory(root._, root.Backbone.Form);
+  }
+})(this, function(_, Form) {
+  Form.validators.multiple = function(options) {
+    var base_validator;
+    options = _.extend({
+      separator: ';'
+    }, options);
+    base_validator = Form.validators[options.base_type](options);
+    return function(value) {
+      var out;
+      if (value.trim() === '') {
+        value = null;
+      }
+      if (value == null) {
+        return;
+      }
+      options.value = value;
+      value = value.split(options.separator).map(function(email) {
+        return email.trim();
+      });
+      out = _.compact(_.map(value, base_validator));
+      if (out.length > 0) {
+        return {
+          type: options.base_type,
+          message: options.message || out[0].message
+        };
+      }
+    };
+  };
+  Form.validators.errMessages.phone = 'Incorrect phone number';
+  Form.validators.phone = function(options) {
+    options = _.extend({
+      type: 'phone',
+      message: Form.validators.errMessages.phone,
+      regexp: /^(?:(?:\(?(?:00|\+)(?:[1-4]\d{2}|[1-9]\d?)\)?)?[\-\. \\\/]?)?(?:\(?\d+\)?[\-\. \\\/]?)+(?:[\-\. \\\/]?(?:\#|ext\.?|extension|x)[\-\. \\\/]?\d+)?$/
+    }, options);
+    return Form.validators.regexp(options);
+  };
+  Form.validators.errMessages.maxlength = _.template('The maximum length is <%= maxlength %> characters', null, Form.templateSettings);
+  Form.validators.maxlength = function(options) {
+    options = _.extend({
+      type: 'maxlength',
+      maxlength: 0,
+      message: Form.validators.errMessages.maxlength
+    }, options);
+    options.regexp = new RegExp("^.{0," + options.maxlength + "}$");
+    return Form.validators.regexp(options);
+  };
+});
